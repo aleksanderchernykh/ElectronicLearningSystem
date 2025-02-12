21e32f84f4ecff2e4b36f160f9dc903a557f2538
Aleksander Chernykh <aleksandr.chernykh3@gmail.com>
2025-02-12 20:13:15 +0300

    Добавление ветки dev

--- ElectronicLearningSystem/ElectronicLearningSystemWebApi/Controllers/AuthController.cs
+++ ElectronicLearningSystem/ElectronicLearningSystemWebApi/Controllers/AuthController.cs
@@ -4,7 +4,6 @@ using ElectronicLearningSystemWebApi.Models.ErrorModel;
 using ElectronicLearningSystemWebApi.Attributes;
 using ElectronicLearningSystemWebApi.Helpers.Services;
 using ElectronicLearningSystemWebApi.Models.UserModel.DTO;
-using System.Runtime.CompilerServices;
 using ElectronicLearningSystemWebApi.Models;
 
 namespace ElectronicLearningSystemWebApi.Controllers
